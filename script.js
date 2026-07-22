1ea6bca31f4aceffdf22136f535d6e24172a07ff
Me.arts <sijuadekehinde4@gmail.com>
2026-07-22 06:27:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+// Navbar background on scroll
 window.addEventListener("scroll", () => {
     const nav = document.querySelector("nav");
 
     if (window.scrollY > 40) {
-        nav.style.background = "#111";
+        nav.style.background = "rgba(15,15,19,0.95)";
     } else {
         nav.style.background = "#181820";
     }
+});
+
+// Scroll animation
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+},{
+    threshold:0.15
+});
+
+document.querySelectorAll("section").forEach(section=>{
+    observer.observe(section);
 });
