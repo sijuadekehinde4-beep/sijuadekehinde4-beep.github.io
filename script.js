a9de9f3155a6183c4ff470b998a086da31576f7a
Me.arts <sijuadekehinde4@gmail.com>
2026-07-22 06:11:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+window.addEventListener("scroll", () => {
+    const nav = document.querySelector("nav");
 
+    if (window.scrollY > 40) {
+        nav.style.background = "#111";
+    } else {
+        nav.style.background = "#181820";
+    }
+});
